7d813c1745e37412d47abfc7d8f3dd8049ec452f
Farhan Raza <razafarhan967@gmail.com>
2025-06-10 13:57:35 +0500

    Create Indices.cpp

---
+++ Indices.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+string my_string = "Hello!";
+char character = my_string.at(1); // or my_string[1]
+
+cout << character << endl;
+  
+  return 0;
+  
+}
