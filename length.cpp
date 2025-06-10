9fc8629b833e5aaa97810e570cc43a4217786e85
Farhan Raza <razafarhan967@gmail.com>
2025-06-10 13:55:04 +0500

    Create length.cpp

---
+++ length.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+string my_string = "Hello";
+int len_string = my_string.length(); // number of char
+
+cout << len_string << endl;
+ 
+  return 0;
+  
+}
