3702f82dffd9e2e3efa9c83c0ff081e7d4541457
Farhan Raza <razafarhan967@gmail.com>
2025-06-10 15:08:21 +0500

    Create replace function.cpp

---
+++ replace function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string string1 = "Hello world!";
+string string2 = "Codio.";
+string string3 = string1.replace(6, 5, string2);
+// erase all characters from index 6 plus 5 chars to the right in string1
+// then insert string2 at index 6 within string1
+
+cout << string3 << endl;
+ 
+  return 0;
+  
+}
