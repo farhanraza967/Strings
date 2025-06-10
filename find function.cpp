053d1521798759c4c2f56efb7c89f752f051961d
Farhan Raza <razafarhan967@gmail.com>
2025-06-10 14:14:57 +0500

    Create find function.cpp

---
+++ find function.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string my_string = "The brown dog jumps over the lazy fox.";
+cout << my_string.find("he", 4) << endl; //start at index 4
+  
+  return 0;
+  
+}
