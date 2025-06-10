2b7aba12662e8d62b867a5ecb0d154413a720531
Farhan Raza <razafarhan967@gmail.com>
2025-06-10 14:44:20 +0500

    Create insert function.cpp

---
+++ insert function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+string my_string = "Today is Satur";
+my_string.insert(my_string.length(), "day");
+cout << my_string << endl;
+  
+  return 0;
+  
+}
