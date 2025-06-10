f6f0f3457c77bc8b125cc7714d319102a5479322
Farhan Raza <razafarhan967@gmail.com>
2025-06-10 15:01:17 +0500

    Create toupper and tolower functions.cpp

---
+++ toupper and tolower functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+string my_string = "the big brown dog";
+
+cout << char(toupper(my_string.at(0))) << endl; // print T
+string my_string = "THE BIG BROWN DOG";
+
+cout << char(tolower(my_string.at(1))) << endl; // print h
+  
+  return 0;
+  
+}
