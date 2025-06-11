5a80b73a46de1e747793fab82ed4dcd087e79a01
Farhan Raza <razafarhan967@gmail.com>
2025-06-11 12:39:03 +0500

    Update find function.cpp

--- find function.cpp
+++ find function.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+int main(){
 
 string my_string = "The brown dog jumps over the lazy fox.";
 cout << my_string.find("he", 4) << endl; //start at index 4
