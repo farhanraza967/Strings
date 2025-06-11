64ffc9bfec3dd60131aae78f7ea04e3b43edc5d0
Farhan Raza <razafarhan967@gmail.com>
2025-06-11 12:38:46 +0500

    Update erase function.cpp

--- erase function.cpp
+++ erase function.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+int main(){
   
 string my_string = "Today is my birthday!";
 my_string.erase(4, 3); // type till 4 indices then remove next 3 indices and print remain
