00901455611b26bb6945a5e36970c5c1e88d2efb
Farhan Raza <razafarhan967@gmail.com>
2025-06-12 18:38:09 +0500

    Update and rename Loop and enhanced loop.cpp to Loops and enhanced loop.cpp

--- Loops and enhanced loop.cpp
+++ Loops and enhanced loop.cpp
@@ -7,8 +7,15 @@ for (int i = 0; i < my_string.length(); i++) {
 }
 cout<<endl;
 string my_string1 = "Hello world!";
-for (char c : my_string){
+for (char c : my_string1){
   cout << c;
+}
+  string my_string2 = "Calvin and Hobbes";
+int i = 0;
+
+while (i < my_string2.length()) {
+  cout << my_string2.at(i);
+  i++;
 }
   return 0;
   
