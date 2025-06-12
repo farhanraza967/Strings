7627f9b275da0e3a587ac00d153926671a286816
Farhan Raza <razafarhan967@gmail.com>
2025-06-12 18:54:30 +0500

    Create Simple comparing.cpp

---
+++ Simple comparing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+string string1 = "It's Friday!";
+string string2 = "It's Friday!";
+string string3 = "It's Friday!";
+string string4 = "It's Monday.";
+
+cout << (string3 != string4)<<endl;
+cout << (string1 == string2)<<endl;
+cout << (string1 != string3)<<endl;
+  
+  return 0;
+}
